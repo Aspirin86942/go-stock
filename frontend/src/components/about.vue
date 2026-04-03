21521182331eb48302bc816a22c2e136bb9963df
Aspirin86942 <18017474605@163.com>
2026-04-04 00:34:41 +0800

    feat: remove sponsor UI from settings and about

--- frontend/src/components/about.vue
+++ frontend/src/components/about.vue
@@ -3,22 +3,14 @@
 // preview.css相比style.css少了编辑器那部分样式
 import 'md-editor-v3/lib/preview.css';
 import {h, onBeforeUnmount, onMounted, ref} from 'vue';
-import {CheckUpdate, GetVersionInfo,GetSponsorInfo,OpenURL} from "../../wailsjs/go/main/App";
+import {CheckUpdate, GetVersionInfo, OpenURL} from "../../wailsjs/go/main/App";
 import {EventsOff, EventsOn,Environment} from "../../wailsjs/runtime";
-import {NAvatar, NButton, useNotification,NText} from "naive-ui";
-import { addMonths, format ,parse} from 'date-fns';
-import { zhCN } from 'date-fns/locale';
+import {NAvatar, NButton, useNotification} from "naive-ui";
 const updateLog = ref('');
 const versionInfo = ref('');
 const icon = ref('https://raw.githubusercontent.com/ArvinLovegood/go-stock/master/build/appicon.png');
-const alipay =ref('https://github.com/ArvinLovegood/go-stock/raw/master/build/screenshot/alipay.jpg')
-const wxpay =ref('https://github.com/ArvinLovegood/go-stock/raw/master/build/screenshot/wxpay.jpg')
 const wxgzh =ref('https://github.com/ArvinLovegood/go-stock/raw/dev/build/screenshot/%E6%89%AB%E7%A0%81_%E6%90%9C%E7%B4%A2%E8%81%94%E5%90%88%E4%BC%A0%E6%92%AD%E6%A0%B7%E5%BC%8F-%E7%99%BD%E8%89%B2%E7%89%88.png')
 const notify = useNotification()
-const vipLevel=ref("");
-const vipStartTime=ref("");
-const vipEndTime=ref("");
-const expired=ref(false)
 
 onMounted(() => {
   document.title = '关于软件';
@@ -26,27 +18,8 @@ onMounted(() => {
     updateLog.value = res.content;
     versionInfo.value = res.version;
     icon.value = res.icon;
-    alipay.value=res.alipay;
-    wxpay.value=res.wxpay;
     wxgzh.value=res.wxgzh;
-
-    GetSponsorInfo().then((res) => {
-      vipLevel.value = res.vipLevel;
-      vipStartTime.value = res.vipStartTime;
-      vipEndTime.value = res.vipEndTime;
-      //判断时间是否到期
-      if (res.vipLevel) {
-        if (res.vipEndTime < format(new Date(), 'yyyy-MM-dd HH:mm:ss')) {
-          notify.warning({content: 'VIP已到期'})
-          expired.value = true;
-        }
-      }
-    })
-
   });
-
-
-
 })
 onBeforeUnmount(() => {
   notify.destroyAll()
@@ -121,14 +94,10 @@ EventsOn("updateVersion",async (msg) => {
           <n-space vertical >
             <n-image width="100" :src="icon" />
             <h1>
-              <n-badge v-if="!vipLevel"  :value="versionInfo" :offset="[80,10]"  type="success">
+              <n-badge :value="versionInfo" :offset="[80,10]" type="success">
                 <n-gradient-text type="info" :size="50" >go-stock</n-gradient-text>
               </n-badge>
-              <n-badge v-if="vipLevel"  :value="versionInfo" :offset="[70,10]"  type="success">
-                <n-gradient-text :type="expired?'error':'warning'" :size="50" >go-stock</n-gradient-text><n-tag :bordered="false" size="small" type="warning">VIP{{vipLevel}}</n-tag>
-              </n-badge>
             </h1>
-            <n-gradient-text  :type="expired?'error':'warning'" v-if="vipLevel" >vip到期时间：{{vipEndTime}}</n-gradient-text>
             <n-button size="tiny" @click="CheckUpdate(1)"  type="info" tertiary >检查更新</n-button>
             <div style="justify-self: center;text-align: left" >
               <p>自选股行情实时监控，基于Wails和NaiveUI构建的AI赋能股票分析工具</p>
@@ -148,32 +117,6 @@ EventsOn("updateVersion",async (msg) => {
               <p>QQ交流群：<a href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=0YQ8qD3exahsD4YLNhzQTWe5ssstWC89&authKey=usOMMRFtIQDC%2FYcatHYapcxQbJ7PwXPHK9OypTXWzNjAq%2FRVvQu9bj2lRgb%2BSZ3p&noverify=0&group_code=491605333" target="_blank">491605333</a></p>
             </div>
           </n-space>
-          <n-divider title-placement="center">支持💕开源</n-divider>
-          <n-flex justify="center">
-            <n-table  size="small" style="width: 820px">
-              <n-thead>
-                <n-tr>
-                  <n-th>赞助计划</n-th>
-                  <n-th>赞助等级</n-th>
-                  <n-th>权益说明</n-th>
-                </n-tr>
-              </n-thead>
-              <n-tbody>
-                <n-tr>
-                  <n-td>每月 0 RMB</n-td><n-td>vip0</n-td><n-td>🌟 全部功能,软件自动更新(从GitHub下载),自行解决github平台网络问题。</n-td>
-                </n-tr>
-                <n-tr>
-                  <n-td>赞助 18.8 RMB/月<br>赞助 120 RMB/年</n-td><n-td>vip1</n-td><n-td>💕 全部功能,软件自动更新(从CDN下载),更新快速便捷。AI配置指导，提示词参考等</n-td>
-                </n-tr>
-                <n-tr>
-                  <n-td>赞助 28.8 RMB/月<br>赞助 240 RMB/年</n-td><n-td>vip2</n-td><n-td>💕 vip1全部功能,启动时自动同步最近24小时市场资讯(包括外媒简讯)，go-stock Ai助手等(详询作者微信/QQ)💕</n-td>
-                </n-tr>
-                <n-tr>
-                  <n-td>每月赞助 X RMB</n-td><n-td>vipX</n-td><n-td>🧩 更多计划，视go-stock开源项目发展情况而定...(承接GitHub项目README广告推广💖)</n-td>
-                </n-tr>
-              </n-tbody>
-            </n-table>
-          </n-flex>
           <n-divider title-placement="center">关于作者</n-divider>
           <n-space vertical>
 <!--            <h1>关于作者</h1>-->
@@ -181,18 +124,9 @@ EventsOn("updateVersion",async (msg) => {
             <h2><a href="https://github.com/ArvinLovegood" target="_blank">@ArvinLovegood</a></h2>
             <p>一个热爱编程的小白，欢迎关注我的Github/微信公众号</p>
             <n-image width="300" :src="wxgzh" />
-            <p>开源不易，如果觉得好用，可以请作者喝杯咖啡。</p>
-            <n-flex justify="center">
-              <n-image width="200" :src="alipay" />
-              <n-image width="200" :src="wxpay" />
-            </n-flex>
           </n-space>
           <n-divider title-placement="center">鸣谢</n-divider>
           <div style="justify-self: center;text-align: left" >
-            <p>
-              感谢以下捐赠者：
-              <n-gradient-text size="small" type="warning">*晨</n-gradient-text><n-divider vertical />
-            </p>
             <p>
               感谢以下开发者：
               <a href="https://github.com/GiCo001" target="_blank">@Gico</a><n-divider vertical />
@@ -219,35 +153,10 @@ EventsOn("updateVersion",async (msg) => {
             <p>
               本软件基于开源技术构建，使用Wails、NaiveUI、Vue等开源项目。技术上如有问题，可以先向对应的开源社区请求帮助。
             </p>
-            <p>
-              开源不易，本人精力和时间有限，如确实需要一对一技术支持，<i style="color: crimson">请先赞助！</i>联系微信(备注 技术支持)：ArvinLovegood
-            </p>
             <p style="color: #FAA04A">*加微信或者QQ时，请先备注或留言需求(如：<a href="#support">技术支持</a>，功能建议，商业咨询等，否则会被忽略)</p>
-            <n-table id="support">
-              <n-thead>
-                <n-tr>
-                  <n-th>技术支持方式</n-th><n-th>赞助(元)</n-th>
-                </n-tr>
-              </n-thead>
-              <n-tbody>
-                <n-tr>
-                  <n-td>
-                    加 QQ：506808970，微信：ArvinLovegood
-                  </n-td>
-                  <n-td>
-                    100/次
-                  </n-td>
-                </n-tr>
-                <n-tr>
-                  <n-td>
-                    长期技术支持（不限次数，新功能优先体验等）
-                  </n-td>
-                  <n-td>
-                    5000
-                  </n-td>
-                </n-tr>
-              </n-tbody>
-            </n-table>
+            <p id="support">
+              联系方式：QQ 506808970，微信 ArvinLovegood
+            </p>
           </div>
 
         </n-card>
